afdffe89a7a4d78bcece1175945ad4ba2d5b9d60
Abdelrahman Farouk <abdelrahman.farouk56@gmail.com>
2026-09-12 19:10:55 +0300

    feat: Implement CoHereProvider with text processing, generation, and embedding methods

--- src/stores/llm/providers/CoHereProvider.py
+++ src/stores/llm/providers/CoHereProvider.py
@@ -20,6 +20,73 @@ class  CoHereProvider(LLMInterface):
         self.embedding_model_id = None
         self.embedding_size = None
         
+        self.client = cohere.Client(api_key=self.api_key)
+        
+        self.logger = logging.getLogger(__name__)
+        
+    def set_generation_model(self, model_id: str):
+        self.genearation_model_id = model_id
+        
+    def set_embedding_model(self, model_id: str, embedding_size: int):
+        self.embedding_model_id = model_id
+        self.embedding_size = embedding_size
+    
+    def process_text(self, text: str):
+        return text[:self.default_input_max_characters].strip()
+    
+    
+    def generate_text(self, prompt: str, chat_history: list=[], max_output_tokens: int= None, 
+                        temperature: float = None):
+        
+        if not self.client:
+            self.logger.error("CoHere client was not set")
+            return None
+        
+        if not self.genearation_model_id:
+            self.logger.error("Generation model for CoHere was not set")
+            return None
+        
+        max_output_tokens = max_output_tokens if max_output_tokens else self.default_generation_output_tokens
+        temperature = temperature if temperature else self.default_generation_temperature
+
+        
+        response = self.client.chat(
+            model = self.genearation_model_id,
+            chat_history = chat_history,
+            message = self.process_text(prompt),
+            temperature = temperature,
+            max_tokens = max_output_tokens
+        )
+        
+        if not response or not response.text:
+            self.logger.error("Error while generating text with CoHere")
+            return None
+        
+        return response.text
+
+    
+    def construct_prompt(self, prompt, role):
+        return {
+            "role": role,
+            "text": self.process_text(prompt)
+        }
+
+    
+    def embed_text(self, text: str, document_type: str = None):
+        
+        if not self.client:
+            self.logger.error("CoHere client was not set")
+            return None
+
+        if not self.embedding_model_id:
+            self.logger.error("Embedding model for CoHere was not set")
+            return None
+
+
+    
+
+
+        
         
 
                 
